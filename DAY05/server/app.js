503683725c546f8a5eab42955989ca864a96aa43
coco <cont9652@gmail.com>
2024-08-02 13:35:28 +0900

    coco

--- DAY05/server/app.js
+++ DAY05/server/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 // /public/index.html을 보여주기 위한 static 폴더 지정.
 app.use(express.static('public'));
 
-// 사람 데이터 목록 선언
+// 회원 데이터 목록 선언
 const humanList = [
     {no:101, name:'고길동', email:'go@human.com', job:'개발자', age: 24},
     {no:102, name:'허길동', email:'heo@human.com', job:'간호사', age: 27},
@@ -24,7 +24,7 @@ app.get('/human', function(req, res) {
     // ejs 페이지로 렌더링 
     // - views/human.ejs 페이지의 코드를 읽어와서 res.end()에 적용한다.
 
-    var message = "사람 정보 관리 페이지";
+    var message = "회원정보 관리 페이지";
     req.app.render('human', {message, humanList}, function(err, html) {
         res.end(html);
     });
